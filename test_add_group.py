6f0bd507ad9646d3fcd9684468f29b4d31f9b44c
Alexander Geseltin <geseltinav@gmail.com>
2022-10-15 09:40:50 +0300

    Добавлен модуль application Тесты переведены на использование фреймворка pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,93 +1,35 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-from  model.group import Group
+import pytest
+from model.group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-
-    
-    def test_add_group(self):
-        wd = self.wd
-        # Открыть страницу логина
-        self.open_home_page(wd)
-        # Авторизоваться под администратором
-        self.login(wd, 'admin', 'secret')
-        # Открыть страницу групп
-        self.open_groups_page(wd)
-        # Создать группу
-        self.create_group(wd, Group())
-        # Вернуться на страницу групп
-        self.return_to_groups_page(wd)
-        # Разлогиниться
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        # Открыть страницу логина
-        self.open_home_page(wd)
-        # Авторизоваться под администратором
-        self.login(wd, 'admin', 'secret')
-        # Открыть страницу групп
-        self.open_groups_page(wd)
-        # Создать группу c пустыми значениями
-        self.create_group(wd, Group(name='', header='', footer=''))
-        # Вернуться на страницу групп
-        self.return_to_groups_page(wd)
-        # Разлогиниться
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    
-    def tearDown(self):
-        self.wd.quit()
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    # Авторизоваться под администратором
+    app.login('admin', 'secret')
+    # Создать группу
+    app.create_group(Group())
+    # Разлогиниться
+    app.logout()
+
+
+def test_add_empty_group(app):
+
+    # Авторизоваться под администратором
+    app.login('admin', 'secret')
+    # Открыть страницу групп
+    app.create_group(Group(name=' ', header=' ', footer=' '))
+    # Разлогиниться
+    app.logout()
+
+
