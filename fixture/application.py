ce6d30742a5b22461238f099fd85b253e2dabaea
Alexander Geseltin <geseltinav@gmail.com>
2022-11-04 17:44:08 +0300

    Добавлена параметризация из консоли на запуск в различных браузерах, и различных ссылках

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,11 +6,19 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
 
     def login(self, username, password):
@@ -27,7 +34,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/addressbook/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
 
     def destroy(self):
