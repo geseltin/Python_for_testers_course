dce70e10cf0e5bc8d6233af8f5b4535c1b8491e4
Alexander Geseltin <geseltinav@gmail.com>
2022-11-04 16:28:11 +0300

    Добавлена параметризация для теста на создание контакта

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
 
 
 
-def test_add_contact(app):
+test_data = [Contact(first_name=Contact.generate_data(Contact), last_name=Contact.generate_data(Contact),
+                     mid_name=Contact.generate_data(Contact), nickname=Contact.generate_data(Contact),
+                     phone_mobile=Contact.generate_mobile_phone(Contact), phone_home=Contact.generate_home_phone(Contact),
+                     phone_work=Contact.generate_work_phone(Contact), email1=Contact.generate_email(Contact),
+                     email2=Contact.generate_email(Contact), email3=Contact.generate_email(Contact),
+                     address=Contact.generate_address(Contact)) for i in range(3)]
+
+
+@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact(app, contact):
     old_contact_list = app.contact.get_contact_list()
-    contact = Contact(first_name=Contact.generate_data(Contact), last_name=Contact.generate_data(Contact),
-                      mid_name=Contact.generate_data(Contact), nickname=Contact.generate_data(Contact),
-                      phone_mobile=Contact.generate_mobile_phone(Contact), phone_home=Contact.generate_home_phone(Contact),
-                      phone_work=Contact.generate_work_phone(Contact), email1=Contact.generate_email(Contact),
-                      email2=Contact.generate_email(Contact), email3=Contact.generate_email(Contact),
-                      address=Contact.generate_address(Contact))
     app.contact.add_new(contact)
     assert len(old_contact_list) + 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,10 +1,25 @@
+import string
+
 from model.group import Group
+import pytest
+import random
 
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 22)]]
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_group_list = app.group.get_group_list()
-    group = Group(name='test_group', header='group_header', footer='group_footer')
     app.group.create(group)
     assert len(old_group_list) + 1 == app.group.count()
     new_group_list = app.group.get_group_list()
@@ -12,13 +27,8 @@ def test_add_group(app):
     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_group_list = app.group.get_group_list()
-    group = Group(name='', header='', footer='')
-    app.group.create(group)
-    new_group_list = app.group.get_group_list()
-    assert len(old_group_list) + 1 == app.group.count()
-    old_group_list.append(group)
-    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
+
+
+
 
 
