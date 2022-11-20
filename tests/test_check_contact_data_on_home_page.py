16c00cf54253e75ade9b5a8b1e1b8c5820a1da5b
Alexander Geseltin <geseltinav@gmail.com>
2022-11-20 08:44:13 +0300

    фикс

--- tests/test_check_contact_data_on_home_page.py
+++ tests/test_check_contact_data_on_home_page.py
@@ -5,7 +5,7 @@ def test_check_contact_data_on_home_page_and_db(app, db):
     contacts_data_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
     contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
-    assert len(contacts_from_home_page) == len(contacts_from_home_page)
+    assert len(contacts_data_from_db) == len(contacts_from_home_page)
 
     for index in range(len(contacts_data_from_db)):
         assert contacts_from_home_page[index].last_name == contacts_data_from_db[index].last_name
