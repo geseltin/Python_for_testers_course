9e6a94f9a1a241b1106fc25d48f7273102be08aa
Alexander Geseltin <geseltinav@gmail.com>
2022-11-20 08:42:16 +0300

    Рефакторинг теста на сравнение контактов на главной странице

--- tests/test_check_contact_data_on_home_page.py
+++ tests/test_check_contact_data_on_home_page.py
@@ -1,21 +1,23 @@
-
+from model.contact import Contact
 
 
 def test_check_contact_data_on_home_page_and_db(app, db):
-    contacts_data_from_db = db.get_contact_list()
-    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_data_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
-    for contact_from_db in contacts_data_from_db:
-        for contact_from_home_page in contacts_from_home_page:
-            if contact_from_db.id == contact_from_home_page.id:
-                assert contact_from_home_page.last_name == contact_from_db.last_name
-                assert contact_from_home_page.first_name == contact_from_db.first_name
-                assert contact_from_home_page.address == app.contact.delete_spaces_in_address(contact_from_db)
-                assert contact_from_home_page.all_emails_from_homepage == app.contact.merge_emails_like_on_home_page(
-                    contact_from_db)
-                assert contact_from_home_page.all_phones_from_homepage == app.contact.merge_phones_like_on_home_page(
-                    contact_from_db)
-                break
+    assert len(contacts_from_home_page) == len(contacts_from_home_page)
+
+    for index in range(len(contacts_data_from_db)):
+        assert contacts_from_home_page[index].last_name == contacts_data_from_db[index].last_name
+        assert contacts_from_home_page[index].first_name == contacts_data_from_db[index].first_name
+        assert contacts_from_home_page[index].address == \
+               app.contact.delete_spaces_in_address(contacts_data_from_db[index])
+        assert contacts_from_home_page[index].all_emails_from_homepage == \
+               app.contact.merge_emails_like_on_home_page(contacts_data_from_db[index])
+        assert contacts_from_home_page[index].all_phones_from_homepage == \
+               app.contact.merge_phones_like_on_home_page(contacts_data_from_db[index])
+
+
 
 
 
