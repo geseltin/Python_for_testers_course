688d3c9fc186e0be4fd1244e7d0d4b2f46cb9474
Alexander Geseltin <geseltinav@gmail.com>
2022-12-09 09:39:19 +0300

    BDD тест

--- db_connection.py
+++ db_connection.py
@@ -1,17 +1,23 @@
-import pymysql
 from fixture.orm import OrmFixture
-from model.group import Group
+from fixture.db import DbFixture
 
-db = OrmFixture(host='127.0.0.1',
+# db = OrmFixture(host='127.0.0.1',
+#                              user='root',
+#                              password='',
+#                              database='addressbook')
+db = DbFixture(host='127.0.0.1',
                              user='root',
                              password='',
                              database='addressbook')
 
 
 try:
-    list = db.get_contact_list()
-    for i in list:
-        print(i)
-
+    # list = db.get_contact_list()
+    # for i in list:
+    #     print(i)
+    with db.connection.cursor() as cursor:
+        rows = cursor.execute("SELECT * FROM address_in_groups")
+        result = cursor.fetchone()
+        print(result)
 finally:
     pass
